4c4e7cbe860b1b2b11c127cc1a0f66f0f5489ade
Rodrigo López <rlopez4@correo.um.edu.uy>
2024-11-11 08:41:52 -0300

    Change branch

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("This is a demo");
+    System.out.println("This is a demo from another branch");
   }
 }
